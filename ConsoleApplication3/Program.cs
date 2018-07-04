298280ad2739a17f3a7c75e71f5e8cb3f4b0da15
lenovo <lenovo@LENOVO->
2018-07-04 20:18:52 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApplication3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите, пожалуйста ФИО: ");
+            string fullName = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Приветствую тебя, " + fullName);
+
+            Console.Write("Хотите перейти к следующему заданию(да/нет): ");
+            string answer = Console.ReadLine();
+            if (answer == "да")
+            {
+                Console.Clear();
+                Console.WriteLine("Вводите числа через Enter: ");
+                int x;
+                List<int> l = new List<int>();
+                while (Int32.TryParse(Console.ReadLine(), out x))
+                {
+                    l.Add(x);
+                }
+                Console.WriteLine(l.Sum());
+            }
+            else
+            {
+            }
+        }
+    }
+}
